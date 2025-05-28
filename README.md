5edae1fbc4933b0f9d6517c1c757b8fe9acc84a0
Tiago Souza <tiagoadesouza21@gmail.com>
2025-05-28 11:17:43 -0300

    Adicionando arquivos

---
+++ README.md
@@ -0,0 +1,2 @@
+DOCUMENTOS:
+https://drive.google.com/drive/folders/1VV3BbzJcXm1neEhpjbmEd0KY67juPwSM?usp=sharing
---
+++ src/test/java/com/estoque/gerenciador/management/easy/easymanagement/EasymanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estoque.gerenciador.management.easy.easymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasymanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
