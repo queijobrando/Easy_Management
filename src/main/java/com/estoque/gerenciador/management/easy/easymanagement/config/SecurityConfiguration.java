f0efe5de375e700fd4685699e57d243c8c1f73eb
Tiago Azevedo <tiagoadesouza21@gmail.com>
2025-06-04 16:56:29 -0300

    Adicionando UserDetails Interface com usuarios em memoria e criptografia BCrypt na senha

--- src/main/java/com/estoque/gerenciador/management/easy/easymanagement/config/SecurityConfiguration.java
+++ src/main/java/com/estoque/gerenciador/management/easy/easymanagement/config/SecurityConfiguration.java
@@ -6,6 +6,12 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -23,5 +29,27 @@ public class SecurityConfiguration {
                 .authorizeHttpRequests(autorize ->
                         autorize.anyRequest().authenticated()) //tem q estar autenticado para requisições
                 .build();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(10);
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder){
+
+        UserDetails user1 = User.builder()
+                .username("usuario")
+                .password(encoder.encode("123"))
+                .roles("USER")
+                .build();
+
+        UserDetails user2 = User.builder()
+                .username("admin")
+                .password(encoder.encode("321"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1, user2);
     }
 }
